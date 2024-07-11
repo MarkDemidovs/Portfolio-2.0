af94d0e8da0ead298dff7bc2e46af0df705c9117
MarkDemidovs <markdemidovs@proton.me>
2024-07-11 22:42:22 +0300

    Version 5.0 - Complete Skills Section Desktop

--- src/app/components/Skill.tsx
+++ src/app/components/Skill.tsx
@@ -5,16 +5,13 @@ interface SkillProps {
 
 export default function Skill({ title, items }: SkillProps) {
   return (
-    <div className="bg-foreground rounded-2xl max-w-96 min-w-96 h-96 mt-7 ml-10">
+    <div className="bg-foreground rounded-2xl max-w-96 min-w-96 h-96 mt-7 ml-10 p-4 flex flex-col">
       <h3 className="text-bigText w-fit text-xl mx-auto pt-3">{title}</h3>
-
-      <div className="flex justify-center h-full">
-        <ul className="text-offwhite mt-4 flex flex-col justify-between w-full px-4 h-full">
-          {items.map((item, index) => (
-            <li key={index} className="text-center">{item}</li>
-          ))}
-        </ul>
-      </div>
+      <ul className="text-offwhite mt-4 flex flex-col justify-between flex-grow">
+        {items.map((item, index) => (
+          <li key={index} className="flex-1">{item}</li>
+        ))}
+      </ul>
     </div>
   );
 }
