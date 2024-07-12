12f74e0c8f0d48ad04af1ff917eb9ddcdaf52d38
MarkDemidovs <markdemidovs@proton.me>
2024-07-12 18:38:33 +0300

    Version 0.6 - Responsive Skills Section

--- src/app/pages/About.tsx
+++ src/app/pages/About.tsx
@@ -8,12 +8,12 @@ export default function About() {
        <div className="flex max-md:flex-col">
         <div className="flex flex-col items-center">
           
-            <div>
+            <div className="px-2">
                 <Hints />
             </div>
 
             <div className="mt-8">
-                <p className="font-light max-w-96">
+                <p className="font-light max-w-96 px-2">
                 As an accomplished programmer, I derive immense joy from conquering intricate challenges and sharing the wonders of computer science. With 5 years of coding expertise and a journey that began with learning C++ at the age of 9, my fascination with computers sparked at 5 years old, igniting a relentless drive to unravel their endless possibilities and inspire others to do the same.
                 </p>
             </div>
